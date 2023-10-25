227f2b5004053580771b79ec12d02f0958ea5433
MarkoMTP <marko.mtp8@gmail.com>
2023-10-25 21:11:20 +0200

    Did all the buttons and functions, designed, all done

--- java.js
+++ java.js
@@ -1,13 +1,36 @@
 let container = document.getElementById('container');
 
-const buttonStart = document.querySelector('#button')
+const buttonStart = document.querySelector('#start');
+const buttonReset = document.querySelector('#reset');
+const buttonChoose = document.querySelector('#size');
 
 
 
 
+
+
+
+
+
+
+
+ 
+
+
+buttonChoose.addEventListener('click',
 
 
 function createDivs(x) {
+
+
+let userInput = prompt('Size?');
+
+
+
+ x = userInput
+
+
+if(x < 100 ) {
 
     for(let i = 1; i <= x; i++) {
 
@@ -16,6 +39,9 @@ function createDivs(x) {
 div.classList.add('box');
 
 container.appendChild(div);
+
+
+
 
 for ( let i = 1;i <= x;i++){
     var color = [, "#3C9EE7", "#E7993C",
@@ -26,24 +52,34 @@ buttonStart.addEventListener('click', () => { grid.addEventListener('mouseover',
 
 grid.style.backgroundColor = color[(Math.floor(Math.random() * color.length))];
 
-}) })
+}) 
+
+})
 
 
 
 div.appendChild(grid);
 
 }
-}
-
 
 }
 
+} else {
 
-createDivs(32)
+    return alert('Wrong, type a number under 100!')
+}
+
+}
+  );
+
+
 
  
- 
+buttonReset.addEventListener('click', () => {
 
+   container.innerHTML = "";
+
+})
 
 
 
